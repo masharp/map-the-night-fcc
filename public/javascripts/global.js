0ef05afa04549521c9a24f95b419a65f30e09a46
Michael Sharp <msharp.oh@gmail.com>
2016-03-14 20:06:41 -0400

    cross-domain fix

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -27,7 +27,8 @@
         errorElement.classList.remove("hidden");
         errorElement.innerHTML = "Please enter a location! (City, ST)";
       } else {
-        var localAPICall = "http://map-the-night.herokuapp.com/api/location/" + inputVal;
+
+        var localAPICall = window.location.hostname + "/api/location/" + inputVal;
 
         Request(localAPICall, function(error, httpResponse, body) {
           if(error) console.error("ERROR RETRIEVING LOCATION DATA", error);
@@ -64,7 +65,7 @@
     /* component function that allows the user to indicate they will be at a location
         that evening. passed to each "spot" element as a prop */
     reserve: function reserve(location, users) {
-      var localAPIURL = "http://map-the-night.herokuapp.com/api/save";
+      var localAPIURL = window.location.hostname + "/api/save";
 
       if(confirm("Are you sure you'll be stopping by?")) {
         Request.post(localAPIURL, {form: { location: location }}, function(error, httpResponse, body) {
